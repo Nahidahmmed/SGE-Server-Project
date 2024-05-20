932cfc1f4770f8cd863be94be6d3def7fbc002e5
Nahidahmmed <nahidahmmed411@gmail.com>
2024-05-20 19:06:38 +0600

    working on page setup

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,18 +1,52 @@
-import { createContext, useState } from "react"
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { app } from "../Firebase/Firebase";
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+export default function AuthProviders({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export const AuthContext = createContext(null)
-// eslint-disable-next-line react/prop-types
-export default function AuthProviders({children}) {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const authInfo = {
-        user,
-        loading
-    }
+  const logOut = () =>{
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("currentuser", currentUser);
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    logOut
+  };
   return (
-    <AuthContext.Provider value={authInfo}>
-        {children}
-    </AuthContext.Provider>
-  )
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,13 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Layout from './Layout/Layout';
+// import Layout from './Layout/Layout';
 import { LoginPage } from './Pages/LoginPage/LoginPage';
 import AuthProviders from './Providers/AuthProviders';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
+import RegistrationForm from './Pages/RegistrationForm/RegistrationForm';
+import Layout from './Layout/Layout';
 
 
 const router = createBrowserRouter([
@@ -18,10 +22,24 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
+        element: <PrivateRoute><RegistrationForm/></PrivateRoute>
+      },
+      {
+        path: "/login",
         element: <LoginPage/>
       }
     ]
   },
+  {
+    path: "/dashboard",
+    element: <PrivateRoute><Dashboard/></PrivateRoute>,
+    // children: [
+    //   {
+    //     path: "/login",
+    //     element: <LoginPage/>
+    //   }
+    // ]
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
